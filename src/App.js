652b85f6dc3e0c826ea1535b100274de92066a2f
Geidar <tufanov.geidar@mail.ru>
2024-05-11 13:49:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Routes, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route />
+      <Route />
+      <Route />
+      <Route />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
